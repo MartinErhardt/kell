fa07d31bdf7de4736772a08d79c851b4f50111f0
Martin Erhardt <martin.erhardt.tu@gmail.com>
2022-06-01 19:09:34 +0200

    +control operators; +basic word expansion

--- Lexer.hs
+++ Lexer.hs
@@ -4,10 +4,9 @@ module Lexer
 import Text.Parsec
 -- TODO No proper wchar support
 import Text.Parsec.String (Parser)
-
--- For info see
--- https://pubs.opengroup.org/onlinepubs/009604499/utilities/xcu_chap02.html#:~:text=command%20is%20parsed.-,2.10.2,-Shell%20Grammar%20Rules
+import Data.Stack 
 data Token = Word String
+  -- operators as in https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#:~:text=command%20is%20parsed.-,2.10.2,-Shell%20Grammar%20Rules
   | AND_IF   -- &&
   | OR_IF    -- ||
   | DSEMI    -- ;;
@@ -30,6 +29,12 @@ data Token = Word String
   | While    -- while
   | Until    -- until
   | For      -- for
+  -- other control operators as in https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_113
+  | Ampersand
+  | LBracket
+  | RBracket
+  | SEMI
+  | PIPE
   | NEWLINE
   | EOF
 
@@ -37,58 +42,75 @@ data Token = Word String
   deriving (Show, Eq)
 
 parseReservedOp :: [Token] -> Parser [Token]
-parseReservedOp front =foldl1 (<|>) ((\(a,b)-> try $ string a >> ( return $ front ++ [b]) ) <$> reservedOps) 
-  where reservedOps=[("&&",AND_IF)
-                    ,("||", OR_IF)
-                    ,(";;", DSEMI)
-                    ,("<<", DLESS)
-                    ,(">>", DGREAT)
-                    ,("<&", LESSAND)
-                    ,(">&", GREATAND)
-                    ,("<>", LESSGREAT)
-                    ,("<<-", DLESSDASH)
-                    ,(">|", CLOBBER)
-                    ,("if", If)
-                    ,("then", Then)
-                    ,("else", Else)
-                    ,("elif", Elif)
-                    ,("fi", Fi)
-                    ,("done", Done)
-                    ,("do", Do)
-                    ,("case", Case)
-                    ,("esac", Esac)
+parseReservedOp front = foldl1 (<|>) ((\(a,b)-> try $ string a >> ( return $ front ++ [b]) ) <$> reservedOps) 
+  where reservedOps=[("&&",    AND_IF)
+                    ,("||",    OR_IF)
+                    ,(";;",    DSEMI)
+                    ,("<<",    DLESS)
+                    ,(">>",    DGREAT)
+                    ,("<&",    LESSAND)
+                    ,(">&",    GREATAND)
+                    ,("<>",    LESSGREAT)
+                    ,("<<-",   DLESSDASH)
+                    ,(">|",    CLOBBER)
+                    ,("if",    If)
+                    ,("then",  Then)
+                    ,("else",  Else)
+                    ,("elif",  Elif)
+                    ,("fi",    Fi)
+                    ,("done",  Done)
+                    ,("do",    Do)
+                    ,("case",  Case)
+                    ,("esac",  Esac)
                     ,("while", While)
                     ,("until", Until)
-                    ,("for", For)]
+                    ,("for",   For)
+                    ,("(",     LBracket)
+                    ,(")",     RBracket)
+                    ,(";",     SEMI)
+                    ,("|",     PIPE)]
 
  
 comment :: Parser [Token] -> Parser [Token] -> Parser [Token]
-comment callback eofA = eofA <|> (char '\n' >> callback) <|> (anyChar   >> (comment callback eofA))
+comment callback eofA = eofA <|> (char '\n' >> callback) <|> (anyChar >> (comment callback eofA))
 
-appendChar :: Char -> [Token] -> Parser [Token]
-appendChar c ((Word r):o) = return $ [Word $ [c] ++ r] ++ o
+appendStr :: String -> [Token] -> Parser [Token]
+appendStr s ((Word r):o) = return $ [Word $ s ++ r] ++ o
 
 escape :: Parser [Token] -> Parser [Token]
 escape callback = (anyChar >>= (\c2 -> callback >>= (\((Word r):o) -> return $ [Word $ ['\\',c2] ++ r] ++ o ) ) )
 
-quote :: Char -> Parser [Token]
-quote mark = let eofA = (eof >> unexpected("mising quote end") ) in eofA
-            <|> (char '\\' >> (eofA <|> escape (quote mark) ) )
-            <|> (char mark >> parseWord >>= appendChar mark )
-            <|> (char '#'  >> comment (quote mark) eofA ) 
-            <|> (anyChar   >>= (\c -> (quote mark) >>= appendChar c ) ) 
+quote :: Parser [Token] -> Parser [Token]
+quote endCondition = let eofA = (eof >> unexpected("mising quote end") ) in eofA <|> endCondition
+            <|> (char '\\' >> (eofA <|> escape (quote endCondition) ) )
+            <|> (char '#'  >> comment (quote endCondition >>= appendStr "\n") eofA ) 
+            <|> (anyChar   >>= (\c -> (quote endCondition) >>= appendStr [c] ) )
 
+
+wordExpansion :: Stack String -> Parser [Token]
+wordExpansion s = (foldl1 (<|>) (stackHandler <$> stackAction s)) -- Pattern matching will fail if string is empty
+  where closingAction s c = if stackIsEmpty s || ( (stackPeek s) /= (Just c)) then Nothing else (\(Just s)-> Just $ fst s) $ stackPop s
+        stackAction s = [("$((", Just $ stackPush s "))")
+                        ,("$(",  Just $ stackPush s ")")
+                        ,("${",  Just $ stackPush s "}")
+                        ,("))",  closingAction s "))")
+                        ,(")",   closingAction s ")")
+                        ,("}",   closingAction s "}")]
+        stackHandler (str, (Just a)) =try $ (if stackIsEmpty a then string str >> parseWord else try $ string str >> quote (wordExpansion a) ) >>= appendStr str
+        stackHandler (str, Nothing) = unexpected("unexpected " ++ str)
+        
 parseWord :: Parser [Token]
 parseWord = let eofA = (eof >> return [Word "", NEOF])
                 endNewline = (eof >> (return $ [Word "",NEWLINE,EOF])) <|> (return $ [Word "",NEWLINE] ) in eofA
             <|> parseReservedOp [Word ""]
             <|> (char ' '  >> ((eof >> (return $ [Word "",EOF])) <|> (return $ [Word ""] ) ) ) -- parse delimiter NOTE: delimiter will be removed later 
-            <|> (char '\n' >> endNewline)
+            <|> (char '\n' >> endNewline )
             <|> (char '\\' >> (eofA <|> escape parseWord) )                                    -- parse quotes
-            <|> (char '\'' >> quote '\''       >>= appendChar '\''  ) 
-            <|> (char '"'  >> quote '"'        >>= appendChar '"'  )
+            <|> (char '\'' >> quote (char '\''>> (parseWord >>= appendStr "'" ) ) >>= appendStr "'") 
+            <|> (char '"'  >> quote (char '"' >> (parseWord >>= appendStr "\"") ) >>= appendStr "\""  )
+            <|> wordExpansion stackNew                                                         -- word expansion
             <|> (char '#'  >> comment endNewline eofA )                                        -- parse comment
-            <|> (anyChar   >>= (\c-> parseWord >>= appendChar c ) )                            -- parse letter
+            <|> (anyChar   >>= (\c-> parseWord >>= appendStr [c] ) )                           -- parse letter
 
 lexSingle = parseReservedOp [] <|> parseWord
 
