9ac8e4c870d5521e0972af7c2820fb7f1c209222
Martin Erhardt <martin.erhardt.tu@gmail.com>
2022-06-15 04:15:16 +0200

    +remove escaped newlines

--- hssh.hs
+++ hssh.hs
@@ -5,7 +5,8 @@ import Control.Monad.Trans.State.Lazy
 import System.Exit
 import System.Posix.Process
 import Text.Parsec
+import Data.Text
 
 main :: IO ()
-main = getLine >>= (\cmd -> evalStateT (exec cmd) getDefaultShellEnv) >> main-- >> exitImmediately ExitSuccess
+main = getLine >>= (flip evalStateT getDefaultShellEnv) . exec . unpack . replace (pack "\\\n") (pack "") . pack >> main-- >> exitImmediately ExitSuccess
 ---main = getLine >>= print . parse lexer "stdin" --"stdin" "check if \n newline is \" accureately #\n rep#resented \n\""
