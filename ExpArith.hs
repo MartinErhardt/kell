49f3bc769c95de150a3855d23331305e719aa05c
Martin Erhardt <martin.erhardt.tu@gmail.com>
2022-06-25 01:53:17 +0200

    +Assignment Ops in arithmetic expansions

--- ExpArith.hs
+++ ExpArith.hs
@@ -1,3 +1,17 @@
+--   Copyright 2022 Martin Erhardt
+--
+--   Licensed under the Apache License, Version 2.0 (the "License");
+--   you may not use this file except in compliance with the License.
+--   You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing, software
+--   distributed under the License is distributed on an "AS IS" BASIS,
+--   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--   See the License for the specific language governing permissions and
+--   limitations under the License.
+
 module ExpArith(
   expandArith
 )where
@@ -9,16 +23,13 @@ import Text.Parsec
 import Text.Parsec.String (Parser)
 import Control.Monad.Trans.Class
 
+import qualified Data.Map as Map
 import qualified Text.Parsec.Language as Lang
 import qualified Text.Parsec.Expr as Ex
 import qualified Text.Parsec.Token as Tok
 import qualified Text.Read as Rd
 
 type AParser a = ParsecT String () Shell a
---data Expr = Num Int | Var Id
---  | Ass Id BinOp Expr
---  | Op BinOp Expr Expr
---  deriving (Eq, Show)
 
 bOpMap = [[("*",  (*))]
          ,[("/",  div)]
@@ -56,21 +67,34 @@ lexer = Tok.makeTokenParser style
          }
 
 numb :: AParser Int
-numb      = Tok.natural lexer >>= return . fromIntegral
+numb      = Tok.natural lexer    >>= return . fromIntegral
 
-getVarVal :: AParser Int
-getVarVal = Tok.identifier lexer >>= (lift . getVar) >>= handleVar
+getVal :: String -> AParser Int
+getVal name = (lift . getVar) name >>= handleVar
   where handleVar v = case v of (Just v) -> case Rd.readMaybe v of (Just n) -> return n
                                                                    _        -> unexpected("no integer")
                                 _        -> unexpected("var does not exist")
 
+getVarVal :: AParser Int
+getVarVal = Tok.identifier lexer >>= getVal
+
+assign :: AParser Int
+assign = do
+  toAssign <- Tok.identifier lexer
+  opFunc   <- (foldl1 (<|>) ((\(op,f) -> Tok.reservedOp lexer op >> return f) <$> assignOps))
+  oldval   <- getVal toAssign
+  newval   <- expr >>= return . opFunc oldval
+  lift $ putVar toAssign (show $ newval)
+  return newval
+  where addEq = fmap (\(op,f) -> (op ++ "=",f))
+        assignOps = (++[("=",curry snd)]) . addEq . filter (\(s,_) -> s/= ">" && s/="<") $ concat bOpMap
+
 baseExpr :: AParser Int
-baseExpr = Tok.parens lexer expr <|> getVarVal <|> numb
+baseExpr = Tok.parens lexer expr <|> try(assign) <|> getVarVal <|> numb
 
 expr :: AParser Int
 expr = Ex.buildExpressionParser table baseExpr
   where infixOp x f = Ex.Infix (Tok.reservedOp lexer x >> return f) Ex.AssocLeft
-        table :: [[Ex.Operator String () Shell Int]]
         table = ((uncurry infixOp) <$>) <$> bOpMap
 
 parseExpr :: AParser Int
